3fc386c168992e03c1f384c7a1796f42fd9a5888
UpcraftLP <j-schirra@gmx.de>
2019-01-13 02:03:00 +0100

    initial files

---
+++ src/main/java/com/github/glasspane/sewntotems/SewnTotems.java
@@ -0,0 +1,25 @@
+package com.github.glasspane.sewntotems;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SewnTotems {
+
+    public static final String MODID = "sewn_totems";
+    public static final String MOD_NAME = "Sewn Totems";
+    public static final String VERSION = "${version}";
+
+    private static final Logger log = LogManager.getLogger(MODID);
+
+    public static final ItemGroup CREATIVE_TAB = FabricItemGroupBuilder.build(new Identifier(MODID, "Items"), () -> new ItemStack(Items.DIAMOND));
+
+    public static Logger getLogger() {
+        return log;
+    }
+
+}
---
+++ src/main/java/com/github/glasspane/sewntotems/mixin/init/SewnTotemsLoader.java
@@ -0,0 +1,11 @@
+package com.github.glasspane.sewntotems.mixin.init;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SewnTotemsLoader implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
